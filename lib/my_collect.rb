26be7d6f9936796706aa29547ebe7b5030826972
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 20:57:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 def my_collect(array)
     i=0
     result=[]
@@ -7,4 +8,18 @@ def my_collect(array)
       i+=1
     end
   result
+=======
+def my_collect(languages, students)
+      i=0
+      collection = []
+      while i < languages.size
+         collection << yield(languages[i].upcase)
+         while i < students.size
+           collection << yield(students[i].first)
+           i+=1
+         end
+         i+=1
+       end
+      collection
+>>>>>>> 83db3bb34d41338cd385dd2c0e6459b39ea16503
 end
